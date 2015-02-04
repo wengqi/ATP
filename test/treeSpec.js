9a9de820e34683f409d6bc9a0a3fc4873e61fadc
Qingo <97383102@qq.com>
2015-02-04 14:51:33 +0800

    treeSpec

---
+++ test/treeSpec.js
@@ -0,0 +1,12 @@
+describe('Tree', function () {
+    var Tree = require('./../src/tree'),
+        should = require('should'),
+        files = require('./files');
+    var tree = new Tree(files);
+    describe('InstanceOf', function () {
+        it('should instance', function () {
+            should(tree).be.object
+            should(tree.files).be.Array
+        })
+    })
+});
