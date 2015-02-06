e6546292c109166e5d17ca7b6f6750bdac83d544
Qingo <97383102@qq.com>
2015-02-06 14:18:36 +0800

    doc

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Page = require('./lib/page'),
-    Doc = require('./lib/doc');
+    Doc = require('./lib/doc'),
+    style = require('./lib/style');
 
 exports.Page = Page;
 exports.Doc = Doc;
+exports.style = style;
