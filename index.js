8fa68fa36e57abbfa9ca784ceda89a1ea2f4258d
Qingo <97383102@qq.com>
2015-02-11 15:23:32 +0800

    filer, doc, single, template, tree

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-var Filer = require('./lib/filer'),
-    Single = require('./lib/single'),
-    Doc = require('./lib/doc'),
-    Template = require('./lib/template'),
-    style = require('./lib/style');
+var Doc = require('./lib/doc');
+    Doc.style = require('./lib/style');
 
-module.exports = function AtomPage(files) {
-    var filer = new Filer(files);
-    this.single = new Single(filer.single);
-    this.doc = new Doc(filer.doc);
-    this.template = new Template(filer.template);
-    this.style = style;
-};
+module.exports = Doc;
