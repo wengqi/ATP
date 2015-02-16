27e926aecf3e55b04379bbad5861a4f73ca0f73f
Qingo <97383102@qq.com>
2015-02-16 10:53:42 +0800

    frame

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var Doc = require('./lib/doc');
-    Doc.style = require('./lib/style');
+var Single = require('./lib/view/single'),
+    Doc = require('./lib/view/doc'),
+    Template = require('./lib/view/template'),
+    Filer = require('./lib/wrap/filer');
+ATP.style = require('./lib/style/define');
 
-module.exports = Doc;
+function ATP(files) {
+    var filer = new Filer(files);
+    this.single = new Single(filer.single);
+    this.template = new Template(filer.template);
+    this.doc = new Doc(filer.doc, filer.single.concat(filer.template));
+    this.dist = [];
+    this.concat();
+}
+
+ATP.prototype = {
+    constructor: ATP,
+
+    concat: function () {
+        this.dist = this.dist.concat(this.doc.toSingleDoc(this.single), this.doc.toTemplateDoc(this.template), this.template.posts);
+        return this;
+    }
+};
+
+module.exports = ATP;
--- test/treeSpec.js
+++ test/treeSpec.js
@@ -1,5 +1,5 @@
 describe('Tree', function () {
-    var Tree = require('../lib/tree'),
+    var Tree = require('../lib/data/tree'),
         should = require('should'),
         files = require('./files');
     var tree = new Tree(files);
