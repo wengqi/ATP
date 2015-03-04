c7d8d3894c199524eedb67a847e3b5eec810907b
Qingo <97383102@qq.com>
2015-03-04 10:33:09 +0800

    gulp-atp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var ATP  = require('./lib/wrap/gulp-ATP');
+var ATP = require('./lib/view/ATP');
+ATP.atp = require('./lib/wrap/gulp-ATP');
 ATP.style = require('./lib/style/define');
 
 module.exports = ATP;
