b6e4a1f1b62040a13e03a4bf17e1925efe26ca06
Qingo <97383102@qq.com>
2015-03-24 11:29:07 +0800

    template

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var config = require('./lib/wrap/gulp-config'),
     doc = require('./lib/wrap/gulp-doc'),
-    define = require('./lib/wrap/define');
+    define = require('./lib/wrap/define'),
+    Page = require('./lib/view/page'),
+    Frame = require('./lib/frame/frame');
 
 module.exports = {
     config: config,
     doc: doc,
-    define: define
+    define: define,
+    template: Page.template,
+    Frame: Frame
 };
